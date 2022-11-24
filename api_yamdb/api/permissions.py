03f52dc0c83d4b615bbc4fbf23f402263a23911d
msk357 <108631219+msk357@users.noreply.github.com>
2022-11-24 21:35:35 +0300

    Test commit

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -30,3 +30,22 @@ class IsAdminOrSuperUser(BasePermission):
             request.user.is_authenticated and request.user.role == 'admin'
             or request.user.is_superuser
         )
+
+
+class AllowAnyOrIsAdminOrSuperUser(BasePermission):
+    """Test"""
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return (
+            request.user.is_authenticated and request.user.role == 'admin'
+            or request.user.is_superuser
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return (
+            request.user.is_authenticated and request.user.role == 'admin'
+            or request.user.is_superuser
+        )
