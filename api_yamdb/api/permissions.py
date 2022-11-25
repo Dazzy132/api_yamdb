6f67ae9c3ed2225d908f10bba75c18cdad7adb5f
Влад Соловьёв <vladsolo23rus@gmail.com>
2022-11-25 16:34:21 +0300

    Edit permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -42,10 +42,3 @@ class IsAdminOrReadOnly(BasePermission):
             request.user.is_authenticated and request.user.role == 'admin'
             or request.user.is_superuser
         )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in SAFE_METHODS or
-            request.user.is_authenticated and request.user.role == 'admin'
-            or request.user.is_superuser
-        )
